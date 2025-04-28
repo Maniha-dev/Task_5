b5717874cf37307338b51e19c76991f6b10b8f7b
Maniha-dev <nasirrazaptcl@gmail.com>
2025-04-28 01:00:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let w=prompt("Enter your weight")
+let h=prompt("Enter your height")
+let fr=w/h**2
+let dis2=("your height is " +h+  " your weight is " +w )
+console.log(dis2);
+let dis=("Your BMI value is " + fr)
+alert("Your BMI value is " + fr)
+console.log(dis);
